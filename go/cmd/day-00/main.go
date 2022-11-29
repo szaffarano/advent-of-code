534b0054f6cc14a67cb7c9b44231c8ae23dfd730
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2022-11-29 22:34:25 +0100

    feat: Go template

---
+++ go/cmd/day-00/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func partOne(lines []string) string {
+  return fmt.Sprintf("Input length: %d", len(lines))
+}
+
+func partTwo(lines []string) string {
+  return fmt.Sprintf("Input length: %d", len(lines))
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, fmt.Sprintf("Usage %s <input>", os.Args[0]))
+		os.Exit(1)
+	}
+
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+  fmt.Printf("Result One: %s\n", partOne(lines))
+  fmt.Printf("Result Two: %s\n", partTwo(lines))
+}
+
+
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/szaffarano/advent-of-code-2022/go
+
+go 1.19
