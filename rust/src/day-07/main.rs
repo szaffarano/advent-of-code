0529cdcf9ab88072640be045b57805df07694ee5
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2022-12-14 12:10:20 +0100

    chore: remove broken code

--- rust/src/day-07/main.rs
+++ rust/src/day-07/main.rs
@@ -1,59 +1,7 @@
 use rust::get_input;
 use std::env;
 
-#[derive(Debug)]
-struct Filesystem {
-    root: Box<Dir>,
-    working_directory: Box<Dir>,
-}
-
-    impl Filesystem {
-        pub fn new() -> Filesystem {
-        let root = Box::from(Dir::new("/"));
-        Filesystem {
-            root: root,
-            working_directory: root,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct File {
-    name: String,
-    size: usize,
-}
-
-impl File {
-    pub fn new(name: &str, size: usize) -> File {
-        File {
-            name: String::from(name),
-            size,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Dir {
-    name: String,
-    files: Vec<File>,
-    children: Vec<Dir>,
-}
-
-impl Dir {
-    pub fn new(name: &str) -> Dir {
-        Dir {
-            name: String::from(name),
-            files: Vec::new(),
-            children: Vec::new(),
-        }
-    }
-}
-
 fn part_one(input: &Vec<String>) -> String {
-    let mut filesystem = Dir::new("/");
-
-    filesystem.files.push(File::new("A", 10));
-
     format!("Input length: {}", input.len())
 }
 
