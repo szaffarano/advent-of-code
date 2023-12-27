7b9776c94f293fa565cf51cab9f678cea462c4b9
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-12-27 13:49:17 +0100

    chore: Day 7 structure

---
+++ 2023/bin/day07/day07.ml
@@ -0,0 +1,14 @@
+open Core
+
+let part_one _ = 0
+let part_two _ = 0
+
+let () =
+  (* let input = "./data/day07-test.txt" in *)
+  let input = "./data/day07.txt" in
+  let data = Advent.read_lines input in
+  (* Part one: ?? *)
+  part_one data |> printf "Part one: %d\n";
+  (* Part two: ?? *)
+  part_two data |> printf "Part two: %d\n"
+;;
