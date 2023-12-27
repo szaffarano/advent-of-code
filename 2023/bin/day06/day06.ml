258f626e15f1ca137af5ba3189aac69080710510
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-12-27 12:41:47 +0100

    feat: Day six

--- 2023/bin/day06/day06.ml
+++ 2023/bin/day06/day06.ml
@@ -33,14 +33,32 @@ let part_one data =
   |> List.fold ~init:1 ~f:( * )
 ;;
 
-let part_two data = List.length data
+let part_two data =
+  let chances =
+    parse_input data
+    |> List.fold ~init:("", "") ~f:(fun acc race ->
+      let time = fst acc in
+      let distance = snd acc in
+      time ^ Int.to_string race.time, distance ^ Int.to_string race.distance)
+    |> fun (time, distance) ->
+    { time = Int.of_string time; distance = Int.of_string distance }
+    |> fun race ->
+    List.range 1 race.time
+    |> List.map ~f:(fun i ->
+      let remaining = race.time - i in
+      let speed = i in
+      remaining * speed)
+    |> List.filter ~f:(fun chance -> chance > race.distance)
+  in
+  chances |> List.length
+;;
 
 let () =
   (* let input = "./data/day06-test.txt" in *)
   let input = "./data/day06.txt" in
   let data = Advent.read_lines input in
-  (* Part one: ?? *)
+  (* Part one: 1195150 *)
   part_one data |> printf "Part one: %d\n";
-  (* Part two: ?? *)
+  (* Part two: 42550411 *)
   part_two data |> printf "Part two: %d\n"
 ;;
