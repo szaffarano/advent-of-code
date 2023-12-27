ed1d34e57c05426d66e14487892261a2b7a31650
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2023-12-27 12:45:02 +0100

    chore: Code style

--- 2023/bin/day06/day06.ml
+++ 2023/bin/day06/day06.ml
@@ -5,13 +5,13 @@ type race =
   ; distance : int
   }
 
-let parse_input a =
+let parse_input input =
   let parse_line prefix line =
     String.substr_replace_all line ~pattern:prefix ~with_:""
     |> String.split ~on:' '
     |> List.filter ~f:(fun s -> String.length s > 0)
   in
-  match a with
+  match input with
   | [ raw_time; raw_distance ] ->
     let time = parse_line "Time:" raw_time in
     let distance = parse_line "Distance:" raw_distance in
@@ -20,37 +20,39 @@ let parse_input a =
   | _ -> failwith "invalid input"
 ;;
 
+let distance_for race charging_time =
+  let remaining_time = race.time - charging_time in
+  let speed = charging_time in
+  remaining_time * speed
+;;
+
 let part_one data =
   parse_input data
   |> List.map ~f:(fun race ->
     List.range 1 race.time
-    |> List.map ~f:(fun i ->
-      let remaining = race.time - i in
-      let speed = i in
-      remaining * speed)
-    |> List.filter ~f:(fun chance -> chance > race.distance)
-    |> List.length)
+    |> List.map ~f:(fun charging_time -> distance_for race charging_time)
+    |> List.fold ~init:0 ~f:(fun acc chance ->
+      if chance > race.distance then acc + 1 else acc))
   |> List.fold ~init:1 ~f:( * )
 ;;
 
 let part_two data =
-  let chances =
-    parse_input data
+  let fix_input races =
+    races
     |> List.fold ~init:("", "") ~f:(fun acc race ->
       let time = fst acc in
       let distance = snd acc in
       time ^ Int.to_string race.time, distance ^ Int.to_string race.distance)
     |> fun (time, distance) ->
     { time = Int.of_string time; distance = Int.of_string distance }
-    |> fun race ->
-    List.range 1 race.time
-    |> List.map ~f:(fun i ->
-      let remaining = race.time - i in
-      let speed = i in
-      remaining * speed)
-    |> List.filter ~f:(fun chance -> chance > race.distance)
   in
-  chances |> List.length
+  parse_input data
+  |> fix_input
+  |> fun race ->
+  List.range 1 race.time
+  |> List.map ~f:(fun charging_time -> distance_for race charging_time)
+  |> List.fold ~init:0 ~f:(fun acc chance ->
+    if chance > race.distance then acc + 1 else acc)
 ;;
 
 let () =
