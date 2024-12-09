5b6d730ace7ea7eb5b86218daccff822c44e6de1
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-12-09 06:20:12 +0100

    Day 09 scaffolding

---
+++ 2024/src/main/java/ar/zaffa/aoc/puzzles/Day09.java
@@ -0,0 +1,23 @@
+package ar.zaffa.aoc.puzzles;
+
+import static ar.zaffa.aoc.annotations.Solution.Day.DAY09;
+import static ar.zaffa.aoc.annotations.Solution.Part.PART1;
+import static ar.zaffa.aoc.annotations.Solution.Part.PART2;
+
+import ar.zaffa.aoc.annotations.Solution;
+import java.nio.file.Path;
+
+@SuppressWarnings("unused")
+public class Day09 {
+  private Day09() {}
+
+  @Solution(day = DAY09, part = PART1)
+  public static int part1(Path input) {
+    return 0;
+  }
+
+  @Solution(day = DAY09, part = PART2)
+  public static int part2(Path input) {
+    return 0;
+  }
+}
--- 2024/src/test/java/ar/zaffa/aoc/PuzzlesTest.java
+++ 2024/src/test/java/ar/zaffa/aoc/PuzzlesTest.java
@@ -9,6 +9,7 @@ import static ar.zaffa.aoc.annotations.Solution.Day.DAY05;
 import static ar.zaffa.aoc.annotations.Solution.Day.DAY06;
 import static ar.zaffa.aoc.annotations.Solution.Day.DAY07;
 import static ar.zaffa.aoc.annotations.Solution.Day.DAY08;
+import static ar.zaffa.aoc.annotations.Solution.Day.DAY09;
 import static ar.zaffa.aoc.annotations.Solution.Part;
 import static ar.zaffa.aoc.annotations.Solution.Part.PART1;
 import static ar.zaffa.aoc.annotations.Solution.Part.PART2;
@@ -28,42 +29,46 @@ class PuzzlesTest {
 
   private static Stream<Arguments> checkExample() {
     return Stream.of(
-        Arguments.of(DAY01, PART1, 11),
-        Arguments.of(DAY01, PART2, 31),
-        Arguments.of(DAY02, PART1, 2),
-        Arguments.of(DAY02, PART2, 4),
-        Arguments.of(DAY03, PART1, 161),
-        Arguments.of(DAY03, PART2, 48),
-        Arguments.of(DAY04, PART1, 18),
-        Arguments.of(DAY04, PART2, 9),
-        Arguments.of(DAY05, PART1, 143),
-        Arguments.of(DAY05, PART2, 123),
-        Arguments.of(DAY06, PART1, 41),
-        Arguments.of(DAY06, PART2, 6),
-        Arguments.of(DAY07, PART1, 3749L),
-        Arguments.of(DAY07, PART2, 11387L),
+        Arguments.of(DAY09, PART2, 0),
+        Arguments.of(DAY09, PART1, 0),
+        Arguments.of(DAY08, PART2, 34),
         Arguments.of(DAY08, PART1, 14),
-        Arguments.of(DAY08, PART2, 34));
+        Arguments.of(DAY07, PART2, 11387L),
+        Arguments.of(DAY07, PART1, 3749L),
+        Arguments.of(DAY06, PART2, 6),
+        Arguments.of(DAY06, PART1, 41),
+        Arguments.of(DAY05, PART2, 123),
+        Arguments.of(DAY05, PART1, 143),
+        Arguments.of(DAY04, PART2, 9),
+        Arguments.of(DAY04, PART1, 18),
+        Arguments.of(DAY03, PART2, 48),
+        Arguments.of(DAY03, PART1, 161),
+        Arguments.of(DAY02, PART2, 4),
+        Arguments.of(DAY02, PART1, 2),
+        Arguments.of(DAY01, PART2, 31),
+        Arguments.of(DAY01, PART1, 11));
   }
 
   private static Stream<Arguments> checkFinalSolution() {
     return Stream.of(
-        Arguments.of(DAY01, PART1, 1580061),
-        Arguments.of(DAY01, PART2, 23046913),
-        Arguments.of(DAY02, PART1, 606),
-        Arguments.of(DAY02, PART2, 644),
-        Arguments.of(DAY03, PART1, 164730528),
-        Arguments.of(DAY03, PART2, 70478672),
-        Arguments.of(DAY04, PART1, 2599),
-        Arguments.of(DAY04, PART2, 1948),
-        Arguments.of(DAY05, PART1, 6612),
-        Arguments.of(DAY05, PART2, 4944),
-        Arguments.of(DAY06, PART1, 4883),
-        Arguments.of(DAY06, PART2, 1655),
-        Arguments.of(DAY07, PART1, 12940396350192L),
-        Arguments.of(DAY07, PART2, 106016735664498L),
+        Arguments.of(DAY09, PART2, 0),
+        Arguments.of(DAY09, PART1, 0),
+        Arguments.of(DAY08, PART2, 927),
         Arguments.of(DAY08, PART1, 259),
-        Arguments.of(DAY08, PART2, 927));
+        Arguments.of(DAY07, PART2, 106016735664498L),
+        Arguments.of(DAY07, PART1, 12940396350192L),
+        Arguments.of(DAY06, PART2, 1655),
+        Arguments.of(DAY06, PART1, 4883),
+        Arguments.of(DAY05, PART2, 4944),
+        Arguments.of(DAY05, PART1, 6612),
+        Arguments.of(DAY04, PART2, 1948),
+        Arguments.of(DAY04, PART1, 2599),
+        Arguments.of(DAY03, PART2, 70478672),
+        Arguments.of(DAY03, PART1, 164730528),
+        Arguments.of(DAY02, PART2, 644),
+        Arguments.of(DAY02, PART1, 606),
+        Arguments.of(DAY01, PART2, 23046913),
+        Arguments.of(DAY01, PART1, 1580061));
   }
 
   @ParameterizedTest(name = "{0}, {1}")
