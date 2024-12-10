a0dd3238d6bf113139007754a7755d7b3cdb4124
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-12-10 10:40:31 +0100

    code style

--- 2024/src/main/java/ar/zaffa/aoc/puzzles/Day10.java
+++ 2024/src/main/java/ar/zaffa/aoc/puzzles/Day10.java
@@ -3,14 +3,14 @@ package ar.zaffa.aoc.puzzles;
 import static ar.zaffa.aoc.annotations.Solution.Day.DAY10;
 import static ar.zaffa.aoc.annotations.Solution.Part.PART1;
 import static ar.zaffa.aoc.annotations.Solution.Part.PART2;
-import static java.util.stream.Stream.concat;
+import static ar.zaffa.aoc.common.CollectionUtils.append;
 
 import ar.zaffa.aoc.annotations.Solution;
 import ar.zaffa.aoc.common.IntMatrix;
 import ar.zaffa.aoc.common.Point;
 import ar.zaffa.aoc.common.PuzzleUtils;
 import java.nio.file.Path;
-import java.util.Set;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -28,7 +28,7 @@ public class Day10 {
         .find(START)
         .map(
             startPoint ->
-                trailHeads(matrix, startPoint, Set.of(startPoint)).stream()
+                trailHeads(matrix, startPoint, List.of(startPoint)).stream()
                     .flatMap(trailHead -> trailHead.stream().filter(pp -> matrix.get(pp) == END))
                     .collect(Collectors.toSet())
                     .size())
@@ -41,31 +41,22 @@ public class Day10 {
 
     return matrix
         .find(START)
-        .map(startPoint -> trailHeads(matrix, startPoint, Set.of(startPoint)).size())
+        .map(startPoint -> trailHeads(matrix, startPoint, List.of(startPoint)).size())
         .reduce(0, Integer::sum);
   }
 
-  static Set<Set<Point>> trailHeads(IntMatrix matrix, Point current, Set<Point> path) {
+  static List<List<Point>> trailHeads(IntMatrix matrix, Point current, List<Point> path) {
     if (matrix.isOutOfBoundsFor(current)) {
-      return Set.of();
+      return List.of();
+    } else if (matrix.isInside(current) && matrix.get(current) == END) {
+      return List.of(append(path, current));
+    } else {
+      return Stream.of(current.left(), current.right(), current.down(), current.up())
+          .filter(matrix::isInside)
+          .filter(next -> matrix.get(next) == matrix.get(current) + 1)
+          .flatMap(next -> trailHeads(matrix, next, append(path, next)).stream())
+          .toList();
     }
-
-    if (matrix.isInside(current) && matrix.get(current) == END) {
-      return Set.of(concat(path.stream(), Stream.of(current)).collect(Collectors.toSet()));
-    }
-
-    return Stream.of(current.left(), current.right(), current.down(), current.up())
-        .filter(matrix::isInside)
-        .filter(p -> !path.contains(p))
-        .filter(next -> matrix.get(next) == matrix.get(current) + 1)
-        .flatMap(
-            next ->
-                trailHeads(
-                    matrix,
-                    next,
-                    concat(path.stream(), Stream.of(next)).collect(Collectors.toSet()))
-                    .stream())
-        .collect(Collectors.toSet());
   }
 
   static IntMatrix parseInput(Path input) {
