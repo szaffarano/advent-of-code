232b84146bcb718237aa2234a096dce1734474e9
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-12-11 12:20:11 +0100

    Day 12 scaffolding

--- 2024/src/main/java/ar/zaffa/aoc/puzzles/Day02.java
+++ 2024/src/main/java/ar/zaffa/aoc/puzzles/Day02.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+@SuppressWarnings("unused")
 public class Day02 {
   enum Direction {
     INCREASING,
--- 2024/src/main/java/ar/zaffa/aoc/puzzles/Day03.java
+++ 2024/src/main/java/ar/zaffa/aoc/puzzles/Day03.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@SuppressWarnings("unused")
 public class Day03 {
   record Processor(boolean enabled, int value) {
     public Processor() {
