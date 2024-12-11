df49abd1ee6c326cf3162683f62aa00f0dc4b1d6
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-12-11 08:22:30 +0100

    Code style

--- 2024/src/main/java/ar/zaffa/aoc/puzzles/Day11.java
+++ 2024/src/main/java/ar/zaffa/aoc/puzzles/Day11.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SuppressWarnings("unused")
 public class Day11 {
@@ -19,56 +20,46 @@ public class Day11 {
 
   @Solution(day = DAY11, part = PART1, example = "55312", expected = "189167")
   public static long part1(Path input) {
-    return stones(input).stream().map(s -> numOfSplits(s, 25)).reduce(Long::sum).orElse(0L);
+    return result(input, 25);
   }
 
   @Solution(day = DAY11, part = PART2, example = "65601038650482", expected = "225253278506288")
   public static long part2(Path input) {
-    return stones(input).stream().map(s -> numOfSplits(s, 75)).reduce(Long::sum).orElse(0L);
+    return result(input, 75);
   }
 
-  static Map<Long, Map<Long, Long>> cache = new HashMap<>();
-
-  static Long numOfSplits(Long stone, Integer depth) {
-    if (depth == 0) {
-      return 1L;
-    }
-    var newDepth = depth - 1;
-    if (cache.containsKey(stone) && cache.get(stone).containsKey((long) newDepth)) {
-      return cache.get(stone).get((long) newDepth);
-    }
-    if (stone == 0) {
-      var result = numOfSplits(1L, depth - 1);
-      cache.computeIfAbsent(stone, k -> new HashMap<>()).put((long) newDepth, result);
-      return result;
-    }
-    var str = Long.toString(stone);
-    if (str.length() % 2 == 0) {
-      var result =
-          numOfSplits(parseLong(str.substring(0, str.length() / 2)), depth - 1)
-              + numOfSplits(parseLong(str.substring(str.length() / 2)), depth - 1);
-      cache.computeIfAbsent(stone, k -> new HashMap<>()).put((long) newDepth, result);
-      return result;
-    } else {
-      var result = numOfSplits(stone * 2024L, depth - 1);
-      cache.computeIfAbsent(stone, k -> new HashMap<>()).put((long) newDepth, result);
-      return result;
-    }
+  private static Long result(Path input, int blinks) {
+    return stones(input).stream()
+        .map(s -> numOfSplits(s, blinks, new ConcurrentHashMap<>()))
+        .reduce(Long::sum)
+        .orElse(0L);
   }
 
-  static List<Long> splitStone(Long stone) {
-    if (stone == 0) {
-      return List.of(1L);
+  static Long numOfSplits(Long stone, Integer blinks, Map<Long, Map<Integer, Long>> cache) {
+    if (cache.containsKey(stone) && cache.get(stone).containsKey(blinks)) {
+      return cache.get(stone).get(blinks);
+    }
+
+    Long result;
+    if (blinks == 0) {
+      // no blinks, just one stone
+      result = 1L;
+    } else if (stone == 0) {
+      // rule 1
+      result = numOfSplits(1L, blinks - 1, cache);
     } else {
       var str = Long.toString(stone);
       if (str.length() % 2 == 0) {
-        return List.of(
-            parseLong(str.substring(0, str.length() / 2)),
-            parseLong(str.substring(str.length() / 2)));
+        // rule 2
+        result =
+            numOfSplits(parseLong(str.substring(0, str.length() / 2)), blinks - 1, cache)
+                + numOfSplits(parseLong(str.substring(str.length() / 2)), blinks - 1, cache);
       } else {
-        return List.of(stone * 2024L);
+        // rule 3
+        result = numOfSplits(stone * 2024L, blinks - 1, cache);
       }
     }
+    return cache.computeIfAbsent(stone, k -> new HashMap<>()).computeIfAbsent(blinks, k -> result);
   }
 
   private static List<Long> stones(Path input) {
