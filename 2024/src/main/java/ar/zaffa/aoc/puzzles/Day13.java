cf19b947a29c53da0942697f1d9de330e4b4cab7
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-12-13 07:26:39 +0100

    Day 13 scaffolding

---
+++ 2024/src/main/java/ar/zaffa/aoc/puzzles/Day13.java
@@ -0,0 +1,23 @@
+package ar.zaffa.aoc.puzzles;
+
+import static ar.zaffa.aoc.annotations.Solution.Day.DAY13;
+import static ar.zaffa.aoc.annotations.Solution.Part.PART1;
+import static ar.zaffa.aoc.annotations.Solution.Part.PART2;
+
+import ar.zaffa.aoc.annotations.Solution;
+import java.nio.file.Path;
+
+@SuppressWarnings("unused")
+public class Day13 {
+  private Day13() {}
+
+  @Solution(day = DAY13, part = PART1)
+  public static int part1(Path input) {
+    return 0;
+  }
+
+  @Solution(day = DAY13, part = PART2)
+  public static int part2(Path input) {
+    return 0;
+  }
+}
