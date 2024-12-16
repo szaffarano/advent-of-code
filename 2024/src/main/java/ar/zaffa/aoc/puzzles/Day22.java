fc1adb22d084afefd758666abe6d38c65a062bf3
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2024-12-16 20:11:36 +0100

    chore: Day 22 scaffolding

---
+++ 2024/src/main/java/ar/zaffa/aoc/puzzles/Day22.java
@@ -0,0 +1,34 @@
+package ar.zaffa.aoc.puzzles;
+
+import static ar.zaffa.aoc.annotations.Solution.Day.DAY22;
+import static ar.zaffa.aoc.annotations.Solution.Part.PART1;
+import static ar.zaffa.aoc.annotations.Solution.Part.PART2;
+
+import ar.zaffa.aoc.annotations.Solution;
+import ar.zaffa.aoc.common.PuzzleUtils;
+import java.nio.file.Path;
+
+@SuppressWarnings("unused")
+public class Day22 {
+  private Day22() {}
+
+  @Solution(day = DAY22, part = PART1, example = "-1", expected = "-1")
+  public static int part1(Path input) {
+    var data = PuzzleUtils.lines(input).toList();
+    if (data.size() > 20 || data.isEmpty()) {
+      return -1;
+    }
+
+    return 0;
+  }
+
+  @Solution(day = DAY22, part = PART2, example = "-1", expected = "-1")
+  public static long part2(Path input) {
+    var data = PuzzleUtils.lines(input).toList();
+    if (data.size() > 20 || data.isEmpty()) {
+      return -1;
+    }
+
+    return 0;
+  }
+}
